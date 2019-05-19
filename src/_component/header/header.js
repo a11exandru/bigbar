26d4b7a3848808af5555b779b94b768b2f73ca16
a11exandru <alexpuiu20@yahoo.com>
2019-05-19 15:20:52 +0300

    Update the header and the menu-links.

--- src/_component/header/header.js
+++ src/_component/header/header.js
@@ -3,14 +3,19 @@ const input = document.getElementById('menu');
 const menuLinks = document.querySelector('.menu-links');
 
 // functia deschide/inchide meniu
+
 let showMenu = () => {
     if(input.checked == false) {
         menuLinks.style.top = "0";
         label.classList.add('change');
-        deschis++;
     } else {
         menuLinks.style.top = "-100%";
         label.classList.remove('change');
-        deschis++;
     }
+}
+
+let closeMenu = () => {
+    menuLinks.style.top = "-100%";
+    label.classList.remove('change');
+    input.checked = false;
 }
